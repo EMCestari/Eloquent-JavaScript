217cb2018f3ec7b37c56798bb0b532b5defcec13
Enrico Cestari <ecestari@MBP-ecestari.station>
2023-03-17 11:46:36 +0100

    Update C11

--- Chapter 11 - Asynchronous Programming/EJS-Chapter 11.js
+++ Chapter 11 - Asynchronous Programming/EJS-Chapter 11.js
@@ -1,7 +1,54 @@
-let fifteen = Promise.resolve(15);
-fifteen.then(value => console.log(`Got ${value}`));
+import "./crow-tech";
+
+/* CALLBACKS */
+/*
+One approach is to make functions that perform a slow action take an extra argument, a callback function.
+Slow action is started and, when finished, the callback function is called with the result.
+
+If I want to perform multiple asynchronous actions in a row with callbacks, I have to keep on passing new functions.
+*/
+setTimeout(() => console.log("Tick"),500);
+
+
+/*
+The following code looks up a "food cache" in the storage.
+Nevertheless, indentationn level increases with each asyncrhonous call.
+*/
+bigOak.readStorage("food caches", caches => {
+    let firstCache = caches[0];
+    bigOak.readStorage(firstCache, info => {
+        console.log(info);
+    });
+});
+
+bigOak.send("Cow Pasture", "note", "Let's caw loudly at 7PM", () => console.log("Note delivered."));
+
+// Following function defines a new type of request:
+defineRequestType("note",(nest, content, source, done) => {
+    console.log('${nest.name} received note: ${content}'); // this is to verify that request arrived.
+    done(); // Any function that calls a function that works asynchronously must itself be a synchronous.
+})
+
+// Calling a callback is more involved and error-prone than simply returning a value, so it is better to use another construct.
+
+
+/* PROMISES */
+
+// Instead of arranging for a function to be called at some point in the future, you can return an object that represents
+// the future event. This is a Promise.
+// A Promise is an asynchronous action that MAY complete at some point and produce a value.
+// A promise can notificy anyone who is interested when its value is available
+
+let fifteen = Promise.resolve(15); // Easiest way to create a promise - the value you give is wrapped in a promise
+fifteen.then(value => console.log(`Got ${value}`)); // then method is used to get result of a promise. It registers a callback function to be called when the Promise resolves and produces a value.
 // -> Got 15
 
+// Actually, the "then" method returns another promise, which resolves to the value that the handler function returns or, if it that returns a promise, waits for that promise and then resolves to its result.
+// A promise can be created with a constructor:
+// - It expects a function as argument
+// - It immediately calls the function 
+// - It expects to pass a function that can resolve the promise
+// - Only the code that created the promise can resolve it
 
 function storage(nest,name) {
     return new Promise(resolve => {
@@ -11,6 +58,9 @@ function storage(nest,name) {
 
 storage(bigOak, "enemies")
 .then(value => console.log("Got", value));
+
+// Promises simplify the use of asynchronous functions, as promised-based functions look similar to regular ones:
+// they take input as arguments and return their output - only, that output may not be available yet.
 
 
 new Promise((_,reject) => reject(new Error("Fail")))
@@ -73,7 +123,7 @@ function availableNeighbors(nest){
 
 
 
-import {everywhere} from "./crow-tech";
+import {bigOak, everywhere} from "./crow-tech";
 
 everywhere(nest => {
     nest.state.gossip = [];
@@ -92,3 +142,188 @@ requestType("gossip",(nest, message, source) => {
     console.log(`${nest.name} received gossip '${message}' from ${source}`);
     sendGossip(nest, message, source);
 });
+
+requestType("connections", (nest, {name, neighbors},
+    source) => {
+    let connections = nest.state.connections;
+    if (JSON.stringify(connections.get(name)) ==
+
+
+    JSON.stringify(neighbors)) return;
+    connections.set(name, neighbors);
+    broadcastConnections(nest, name, source);
+});
+
+function broadcastConnections(nest, name, exceptFor = null) {
+    for (let neighbor of nest.neighbors) {
+    if (neighbor == exceptFor) continue;
+        request(nest, neighbor, "connections", {
+            name,
+            neighbors: nest.state.connections.get(name)
+        });
+    }
+}
+
+everywhere(nest => {
+    nest.state.connections = new Map();
+    nest.state.connections.set(nest.name, nest.neighbors);
+    broadcastConnections(nest, nest.name);
+});
+
+
+function findRoute(from, to, connections) {
+    let work = [{at: from, via: null}];
+    for (let i = 0; i < work.length; i++) {
+      let {at, via} = work[i];
+      for (let next of connections.get(at) || []) {
+        if (next == to) return via;
+        if (!work.some(w => w.at == next)) {
+          work.push({at: next, via: via || next});
+        }
+      }
+    }
+  
+    return null;
+  }
+
+
+  function routeRequest(nest, target, type, content) {
+    if (nest.neighbors.includes(target)) {
+      return request(nest, target, type, content);
+    } else {
+      let via = findRoute(nest.name, target,
+                          nest.state.connections);
+      if (!via) throw new Error(`No route to ${target}`);
+      return request(nest, via, "route",
+                     {target, type, content});
+    }
+  }
+  
+  requestType("route", (nest, {target, type, content}) => {
+    return routeRequest(nest, target, type, content);
+  });
+
+
+
+  requestType("storage", (nest, name) => storage(nest, name));
+
+async function findInStorage(nest, name) {
+  let local = await storage(nest, name);
+  if (local != null) return local;
+
+  let sources = network(nest).filter(n => n != nest.name);
+  while (sources.length > 0) {
+    let source = sources[Math.floor(Math.random() *
+                       sources.length)];
+    sources = sources.filter(n => n != source);
+
+    try {
+      let found = await routeRequest(nest, source, "storage",
+                                     name);
+      if (found != null) return found;
+    } catch (_) {}
+  }
+  throw new Error("Not found");
+}
+
+function network(nest) {
+  return Array.from(nest.state.connections.keys());
+}
+
+function findInRemoteStorage(nest, name) {
+  let sources = network(nest).filter(n => n != nest.name);
+  function next() {
+    if (sources.length == 0) {
+      return Promise.reject(new Error("Not found"));
+    } else {
+      let source = sources[Math.floor(Math.random() *
+                           sources.length)];
+      sources = sources.filter(n => n != source);
+      return routeRequest(nest, source, "storage", name)
+        .then(value => value != null ? value : next(),
+              next);
+    }
+  }
+  return next();
+}
+
+
+function* powers(n) {
+  for (let current = n;; current *= n) {
+    yield current;
+  }
+ }
+    
+for (let power of powers(3)) {
+  if (power > 50) break;
+  console.log(power);
+}
+// → 3
+// → 9
+// → 27
+
+
+
+Group.prototype[Symbol.iterator] = function*() {
+       for (let i = 0; i < this.members.length; i++) {
+         yield this.members[i];
+       }
+    };
+
+
+
+try {
+  setTimeout(() => {
+    throw new Error("Woosh");
+  }, 20);
+} catch (_) {
+
+  // This will not run
+  console.log("Caught!");
+}
+
+let start = Date.now();
+setTimeout(() => {
+  console.log("Timeout ran at", Date.now() - start);
+}, 20);
+while (Date.now() < start + 50) {}
+console.log("Wasted time until", Date.now() - start);
+// → Wasted time until 50
+// → Timeout ran at 55
+
+
+
+Promise.resolve("Done").then(console.log);
+console.log("Me first!");
+// → Me first!
+// → Done
+
+
+
+function anyStorage(nest, source, name) {
+      if (source == nest.name) return storage(nest, name);
+    
+      else return routeRequest(nest, source, "storage", name);
+    }
+    
+    async function chicks(nest, year) {
+      let list = "";
+      await Promise.all(network(nest).map(async name => {
+        list += `${name}: ${
+           await anyStorage(nest, name, `chicks in ${year}`)
+        }\n`;
+      }));
+      return list;
+    }
+
+
+
+    async function chicks(nest, year) {
+          let lines = network(nest).map(async name => {
+            return name + ": " +
+              await anyStorage(nest, name, `chicks in ${year}`);
+          });
+          return (await Promise.all(lines)).join("\n");
+        }
+
+
